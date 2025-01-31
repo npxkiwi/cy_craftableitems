7928db7419ef1550fb23dcdf7791d4a1e134e31d
Notepad <151445483+npxkiwi@users.noreply.github.com>
2025-01-31 20:28:46 +0100

    Update cl_fraud.lua

--- client/cl_fraud.lua
+++ client/cl_fraud.lua
@@ -1,3 +1,24 @@
 RegisterNetEvent('cy_craftableitem:closeinv', function()
     exports.ox_inventory:closeInventory()
 end)
+
+lib.callback.register('cy_craftableitem:begincrafting', function(itemName)
+    if lib.progressCircle({
+        duration = 5000,
+        position = 'bottom',
+        useWhileDead = false,
+        canCancel = false,
+        disable = {
+            car = true,
+        },
+        label = 'Laver ' .. itemName .. '...',
+        anim = {
+            dict = 'amb@prop_human_parking_meter@male@idle_a',
+            clip = 'idle_a'
+        },
+    }) then
+        return true
+    else
+        return false
+    end
+end)
