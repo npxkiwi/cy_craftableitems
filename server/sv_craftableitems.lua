d868c182bc898f1039f388a248b102a06e6c703b
Notepad <151445483+npxkiwi@users.noreply.github.com>
2025-01-31 20:28:30 +0100

    Update sv_craftableitems.lua

--- server/sv_craftableitems.lua
+++ server/sv_craftableitems.lua
@@ -1,5 +1,20 @@
+function sendLogs(color, name, message, footer, logs)
+    local embed = {
+          {
+              ["color"] = color,
+              ["title"] = "**".. name .."**",
+              ["description"] = message,
+              ["footer"] = {
+                  ["text"] = footer,
+              },
+          }
+      }
+  
+    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
+end
+
 function craftitems()
-    for k,v in ipairs(Config.CraftItems) do
+    for k, v in ipairs(Config.CraftItems) do
         local craftItem = exports.ox_inventory:registerHook('swapItems', function(payload)
             if payload.action == 'swap' then
                 local fromItem = payload.fromSlot.name
@@ -9,10 +24,15 @@ function craftitems()
                     local removefirst = exports.ox_inventory:RemoveItem(player, v.first_item, v.first_item_amount)
                     local removesecond = exports.ox_inventory:RemoveItem(player, v.second_item, v.second_item_amount)
                     if removefirst and removesecond then
-                        exports.ox_inventory:AddItem(player, v.crafted_item, v.crafted_item_amount)
-                        TriggerClientEvent('cy_craftableitem:closeinv', -1)
+                        lib.callback('cy_craftableitem:begincrafting',player, function(success)
+                            if success then
+                                exports.ox_inventory:AddItem(player, v.crafted_item, v.crafted_item_amount)
+                                TriggerClientEvent('cy_craftableitem:closeinv', player)
+                                sendLogs(7730498, "CY CRAFTABLE ITEMS", "Player: ".. player .. "\nItem 1: " ..v.first_item.. "\nItem 2: "..v.second_item.."\nCrafted: "..v.crafted_item.."\n", "Lavet af Notepad", Config.Craftlog)
+                            end
+                        end, v.crafted_item_progress_name)
                     end
-    
+
                     return false
                 end
             end
@@ -22,6 +42,8 @@ function craftitems()
         })
     end
 end
+
+
 AddEventHandler('onResourceStart', function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
       return
